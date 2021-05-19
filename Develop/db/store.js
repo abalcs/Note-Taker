496852687cbeb465fa5cdcaad41a2e12e1e50509
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-05-19 13:02:15 -0400

    started store.js file

---
+++ Develop/db/store.js
@@ -0,0 +1,10 @@
+const util = require("util");
+const fs = require("fs");
+const uuid = require("uuid/v1");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    
+}
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const apiRoutes = require("./Develop/routes/apiRoutes");
+const htmlRoutes = require("./Develop/routes/htmlRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+})
